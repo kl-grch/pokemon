bd71c835de5a437408f18327af9bf6577cbcc2ac
Nikolay Gerasimov <kolyagerich@gmail.com>
2023-04-28 18:21:44 +0300

    add apple icon

--- components/loader/Loader.js
+++ components/loader/Loader.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
 
 export default function Loader() {
-    return (
-        <div className="loader">
-            <Image
-            src='/images/whos_that_pokemon.png'
-            alt='Loading'
-            width={350}
-            height={350}/>
-        </div>
-    )
-};
+  return (
+    <>
+      <div className="loader">
+        <Image
+          src="/images/whos_that_pokemon.png"
+          alt="Loading"
+          width={350}
+          height={350}
+        />
+      </div>
+      <h2 style={{ textAlign: "center" }}>Loading...</h2>
+    </>
+  );
+}
